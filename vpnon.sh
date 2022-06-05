ef639b8b2209d43cfc28083770ff1efa5ed9355a
Prateek Yadav <55643849+kkYrusobad@users.noreply.github.com>
2022-06-06 04:14:34 +0530

    Update vpnon.sh

--- vpnon.sh
+++ vpnon.sh
@@ -1,28 +1,35 @@
 #!/bin/sh
 
-echo " _____ _        __  __              _           "
-echo "|_   _| |_  ___|  \/  |___ _  _ _ _| |_ ___ _ _ "
-echo "  | | | ' \/ -_) |\/| / _ \ || | ' \  _/ -_) '_|"
-echo "  |_| |_||_\___|_|  |_\___/\_,_|_||_\__\___|_|  "
+GREEN="\e[32m"
+RED="\e[31m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+BOLDMAG="\e[1;35m"
+ENDCOLOR="\e[0m"
+
+echo -e "${BOLDMAG} _____ _        __  __              _           ${ENDCOLOR}"
+echo -e "${BOLDMAG}|_   _| |_  ___|  \/  |___ _  _ _ _| |_ ___ _ _ ${ENDCOLOR}"
+echo -e "${BOLDMAG}  | | | ' \/ -_) |\/| / _ \ || | ' \  _/ -_) '_|${ENDCOLOR}"
+echo -e "${BOLDMAG}  |_| |_||_\___|_|  |_\___/\_,_|_||_\__\___|_|  ${ENDCOLOR}"
 
 ismounted=$(grep kkytest /etc/mtab)
 
 theip=$(/home/kky/.local/bin/custom/ipc)
 
 if [[ "$theip" == "$OCIP" ]]; then
-	echo -e "\nAlready Connected to the server!"
+	echo -e "\n${RED}Already Connected to the server!${ENDCOLOR}"
 else
-	echo -e "\nChecking mount...\n"
+	echo -e "\n${CYAN}Checking mount...${ENDCOLOR}\n"
 
 	if [[ -z $ismounted ]]; then
-		echo "======================================"
-		echo "Not mounted, Starting VPNoc..."
-		echo "======================================"
+		echo -e "${GREEN}======================================${ENDCOLOR}"
+		echo -e "${GREEN}Not mounted, Starting VPNoc...${ENDCOLOR}"
+		echo -e "${GREEN}======================================${ENDCOLOR}"
 		wg-quick up love
 	else
-		echo "======================================"
-		echo "Server's Disk mounted, Unmounting..."
-		echo "======================================"
+		echo -e "${GREEN}======================================${ENDCOLOR}"
+		echo -e "${GREEN}Server's Disk mounted, Unmounting...${ENDCOLOR}"
+		echo -e "${GREEN}======================================${ENDCOLOR}"
 		fusermount -u /home/kky/garbage/notlove
 		echo "DONE (fusermount)"
 		sleep 2s
@@ -31,24 +38,24 @@ else
 		stillmounted=$(grep kkytest /etc/mtab)
 		if [[ -z $stillmounted ]]; then
 			echo -e " not mounted, good\n"
-			echo "=============="
-			echo "VPNin..."
-			echo "=============="
+			echo -e "${GREEN}==============${ENDCOLOR}"
+			echo -e "${GREEN}VPNin...${ENDCOLOR}"
+			echo -e "${GREEN}==============${ENDCOLOR}"
 			wg-quick up love
 			sleep 1s
-			echo -e "\n=============="
-			echo "Mounting..."
-			echo "=============="
+			echo -e "\n${GREEN}==============${ENDCOLOR}"
+			echo -e "${GREEN}Mounting...${ENDCOLOR}"
+			echo -e "${GREEN}==============${ENDCOLOR}"
 			sshfs kkytest@notlove:/home/plexmedia/ /home/kky/garbage/notlove
 			sleep 1s
 			if [[ -z $(grep kkytest /etc/mtab) ]]; then
-				echo "Some problem Occured, manual check requested."
+				echo -e "${RED}Some problem Occured, manual check requested.${ENDCOLOR}"
 			else
 				echo -e "Mounted Succesfully, Directory reads\n"
 			fi
 			ls -la /home/kky/garbage/notlove
 		else
-			echo -e "Unmount problem, manual check requested"
+			echo -e "${RED}Unmount problem, manual check requested${ENDCOLOR}"
 		fi
 	fi
 fi
